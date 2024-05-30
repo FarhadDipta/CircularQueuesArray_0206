4a5d5aad9bb6e882ef7b6d08ad57898ff2bc6fc1
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-05-30 15:23:20 +0700

    menambahkan while didalam fungsi display2

--- circularQueue.cpp
+++ circularQueue.cpp
@@ -68,7 +68,19 @@ public:
 
         cout << "\nElement in the queue are...\n";
 
-        if (FRONT_Position <= REAR_Position) {}
+        if (FRONT_Position <= REAR_Position) {
+            while (FRONT_Position <= REAR_Position) {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+
+            FRONT_Position = 0;
+
+            while (FRONT_Position <= REAR_Position) {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+        }
     }
 };
 
